7201f54162ebd7071d8e02c55c6d20ec2f1cba62
ZeevoX <langer.timothy@gmail.com>
2017-10-20 20:31:43 +0100

    Fix minor bug that prevented compilation

--- app/src/main/java/com/zeevox/octo/Ocquarium.java
+++ app/src/main/java/com/zeevox/octo/Ocquarium.java
@@ -22,7 +22,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import com.android.egg.R;
 
 public class Ocquarium extends Activity {
 
