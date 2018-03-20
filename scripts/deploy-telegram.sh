f771d4e550d5e22719a5d4aba8e2970e4990b719
ZeevoX <langer.timothy@gmail.com>
2018-03-20 22:30:01 +0000

    Improve Travis release naming - Create new Travis build type

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -2,11 +2,11 @@
 
 CHANGELOG="$(./scripts/changelog.sh)"
 
-rm -f app/build/outputs/apk/release/output.json
-APK_NAME_OCTO=`ls -1 app/build/outputs/apk/release/ | tr -d '\n'`
+rm -f app/build/outputs/apk/travis/output.json
+APK_NAME_OCTO=`ls -1 app/build/outputs/apk/travis/ | tr -d '\n'`
 
-curl -F chat_id="-1001357118452" -F sticker="CAADBAAD8R8AAmSKPgABCOk3mg-Zvf0C" https://api.telegram.org/bot${BOT_TOKEN}/sendSticker
-curl -F chat_id="-1001357118452" -F document=@"app/build/outputs/apk/release/${APK_NAME_OCTO}" https://api.telegram.org/bot${BOT_TOKEN}/sendDocument
+#curl -F chat_id="-1001357118452" -F sticker="CAADBAAD8R8AAmSKPgABCOk3mg-Zvf0C" https://api.telegram.org/bot${BOT_TOKEN}/sendSticker
+curl -F chat_id="-1001357118452" -F document=@"app/build/outputs/apk/travis/${APK_NAME_OCTO}" https://api.telegram.org/bot${BOT_TOKEN}/sendDocument
 curl -F chat_id="-1001357118452" -F text="${CHANGELOG}" -F parse_mode="HTML" -F disable_web_page_preview="true" https://api.telegram.org/bot${BOT_TOKEN}/sendMessage
 
 echo $(./scripts/changelog.sh)
