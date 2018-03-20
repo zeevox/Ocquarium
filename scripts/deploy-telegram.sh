e427a8f016c560487841690fa0b17f8ad329f08d
Timothy Langer <langer.timothy@gmail.com>
2018-03-20 20:50:52 +0000

    Enable Travis CI integration (#22)
    * Create .travis.yml
    Based on https://github.com/LawnchairLauncher/Lawnchair/blob/alpha/.travis.yml
    * Update .travis.yml
    Reusing scripts by @paphonb, thank you!
    * Add scripts and encrypted keystore
    * Add signing config
    * Remove 'beta' build type

---
+++ scripts/deploy-telegram.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+CHANGELOG="$(./scripts/changelog.sh)
+<a href=\"https://github.com/${TRAVIS_REPO_SLUG}/compare/${TRAVIS_COMMIT_RANGE}\">View on GitHub</a>"
+
+curl -F chat_id="-1001357118452" -F sticker="CAADBAAD8R8AAmSKPgABCOk3mg-Zvf0C" https://api.telegram.org/bot${BOT_TOKEN}/sendSticker
+curl -F chat_id="-1001357118452" -F document=@"${APK_NAME}" https://api.telegram.org/bot${BOT_TOKEN}/sendDocument
+curl -F chat_id="-1001357118452" -F text="${CHANGELOG}" -F parse_mode="HTML" -F disable_web_page_preview="true" https://api.telegram.org/bot${BOT_TOKEN}/sendMessage
+
+echo $(./scripts/changelog.sh)
