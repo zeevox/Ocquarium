b2bd94bbbc8c1782c5e308a1ded905d9c077e133
harens <harensdeveloper@gmail.com>
2018-08-01 16:28:40 +0100

    Removal of Backtick Legacy Syntax

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -3,7 +3,7 @@
 CHANGELOG="$(./scripts/changelog.sh)"
 
 rm -f app/build/outputs/apk/travis/output.json
-APK_NAME_OCTO=`ls -1 app/build/outputs/apk/travis/ | tr -d '\n'`
+APK_NAME_OCTO=$(ls -1 app/build/outputs/apk/travis/ | tr -d '\n')
 
 #curl -F chat_id="-1001357118452" -F sticker="CAADBAAD8R8AAmSKPgABCOk3mg-Zvf0C" https://api.telegram.org/bot${BOT_TOKEN}/sendSticker
 curl -F chat_id="-1001357118452" -F document=@"app/build/outputs/apk/travis/${APK_NAME_OCTO}" https://api.telegram.org/bot${BOT_TOKEN}/sendDocument
