d9a8d9244eb0c303cacb434f8a4fa7f6667d547b
Haren <harensdeveloper@gmail.com>
2018-10-13 15:28:09 +0100

    Telegram APK Correction

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -17,7 +17,8 @@
 CHANGELOG="$(./scripts/changelog.sh)"
 
 rm -f app/build/outputs/apk/travis/output.json
-APK_NAME_OCTO=$(find -1 app/build/outputs/apk/travis/ | tr -d '\n')
+# shellcheck disable=SC2012
+APK_NAME_OCTO=$(ls -1 app/build/outputs/apk/travis/ | tr -d '\n')
 
 #curl -F chat_id="-1001357118452" -F sticker="CAADBAAD8R8AAmSKPgABCOk3mg-Zvf0C" https://api.telegram.org/bot${BOT_TOKEN}/sendSticker
 curl -F chat_id="-1001357118452" -F document=@"app/build/outputs/apk/travis/${APK_NAME_OCTO}" https://api.telegram.org/bot${BOT_TOKEN}/sendDocument
