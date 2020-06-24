9f523b4033ac11f85495b46d6eda0035d899dffa
Timothy Langer <zeevox.dev@gmail.com>
2020-06-24 22:48:13 +0100

    Revert "Inform user of Android Pie"
    This reverts commit 5e828dee16d395ead09e23deab37347e79eb4fb8.
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- app/src/main/java/com/zeevox/octo/OcquariumActivity.java
+++ app/src/main/java/com/zeevox/octo/OcquariumActivity.java
@@ -37,82 +37,39 @@ import androidx.annotation.Nullable;
 
 public class OcquariumActivity extends Activity {
 
-  /* We place the code in onResume() so that the activity is redrawn after visiting settings. */
-  @Override
-  protected void onResume() {
-    super.onResume();
-    Ocquarium.start(this, getWindow(), getResources(), true);
-
-    PackageManager packageManager = this.getPackageManager();
-    boolean isPieInstalled = false;
-
-    try {
-      packageManager.getPackageInfo("com.zeevox.pie", 0);
-      isPieInstalled = true;
-    } catch (PackageManager.NameNotFoundException e) {
-      e.printStackTrace();
+    /* We place the code in onResume() so that the activity is redrawn after visiting settings. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Ocquarium.start(this, getWindow(), getResources(), true);
     }
 
-    if (!isPieInstalled
-        && Math.random() <= 0.02) { // 2% chance of showing info about Pie easter egg app
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-      AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        if (savedInstanceState == null && PreferenceManager.getDefaultSharedPreferences(this).getBoolean("show_platlogo", false)) {
+            startActivity(new Intent(OcquariumActivity.this, PlatLogoActivity.class));
+        }
 
-      builder.setMessage(R.string.dialog_pie_info_message).setTitle(R.string.dialog_pie_info_title);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N_MR1) {
+            ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
 
-      builder.setPositiveButton(
-          R.string.dialog_pie_info_positive,
-          new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-              startActivity(
-                  new Intent(Intent.ACTION_VIEW)
-                      .setData(Uri.parse(getString(R.string.dialog_pie_info_url))));
-              dialogInterface.dismiss();
+            Intent settingsShortcut = new Intent(this, SettingsActivityV2.class);
+            settingsShortcut.setAction(Intent.ACTION_VIEW);
+
+            ShortcutInfo shortcut =
+                    new ShortcutInfo.Builder(this, "id1")
+                            .setShortLabel(getResources().getString(R.string.action_settings))
+                            .setLongLabel(getResources().getString(R.string.shortcut_settings_long))
+                            .setDisabledMessage(getResources().getString(R.string.shortcut_settings_disabled))
+                            .setIcon(Icon.createWithResource(this, R.drawable.ic_shortcut_settings))
+                            .setIntent(settingsShortcut)
+                            .build();
+
+            if (shortcutManager != null) {
+                shortcutManager.setDynamicShortcuts(Collections.singletonList(shortcut));
             }
-          });
-
-      builder.setNeutralButton(
-          R.string.dialog_pie_info_neutral,
-          new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-              dialogInterface.dismiss();
-            }
-          });
-
-      AlertDialog dialog = builder.create();
-
-      dialog.show();
+        }
     }
-  }
-
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    if (savedInstanceState == null && PreferenceManager.getDefaultSharedPreferences(this).getBoolean("show_platlogo", false)) {
-      startActivity(new Intent(OcquariumActivity.this, PlatLogoActivity.class));
-    }
-
-    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N_MR1) {
-      ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
-
-      Intent settingsShortcut = new Intent(this, SettingsActivityV2.class);
-      settingsShortcut.setAction(Intent.ACTION_VIEW);
-
-      ShortcutInfo shortcut =
-          new ShortcutInfo.Builder(this, "id1")
-              .setShortLabel(getResources().getString(R.string.action_settings))
-              .setLongLabel(getResources().getString(R.string.shortcut_settings_long))
-              .setDisabledMessage(getResources().getString(R.string.shortcut_settings_disabled))
-              .setIcon(Icon.createWithResource(this, R.drawable.ic_shortcut_settings))
-              .setIntent(settingsShortcut)
-              .build();
-
-      if (shortcutManager != null) {
-        shortcutManager.setDynamicShortcuts(Collections.singletonList(shortcut));
-      }
-    }
-  }
 }
